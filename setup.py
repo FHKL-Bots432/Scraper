9de8be847ee21906619ea18273581e8fcb822aad
LɪᴏɴKᴇᴛᴛʏUᴅ <88967484+LionKettyUD@users.noreply.github.com>
2021-12-31 11:16:49 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ def banner():
 	{re}╚═╝{cy}└─┘ ┴ └─┘┴
 	
 	           Version : 1.01
-	{re}Subscribe Termux Professor on Youtube
-	{cy}www.youtube.com/c/TermuxProfessorYT
+	{re}Joine Filmy Hangamaga ON Telegram
+	{cy}https://t.me/Filmy_Hangama 
 	""")
 banner()
 print(gr+"[+] Installing requierments ...")
