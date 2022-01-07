b6e35048cba9bf65ef1ac4c884dad02231b4c1eb
LɪᴏɴKᴇᴛᴛʏUᴅ <88967484+LionKettyUD@users.noreply.github.com>
2022-01-07 18:05:50 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -18,7 +18,7 @@ def banner():
 
               Version : 1.01
  {re}Subscribe Termux Professor on Youtube.
-   {cy}www.youtube.com/c/TermuxProfessorYT
+   {cy}t.me/FHKL_TEAM
         """)
 
 cpass = configparser.RawConfigParser()
